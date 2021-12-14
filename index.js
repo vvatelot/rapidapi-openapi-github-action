93b9d26ee11937b6c353590a95ed8214fda6ac3d
Vincent Vatelot <vincent.vatelot@gmail.com>
2021-12-14 14:20:00 +0100

    chore(action): Add error handling and default servers update
    chore(action): Fix JSON file edit

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const core = require("@actions/core");
 const unirest = require("unirest");
+const fs = require("fs");
 
-const API_URL =
-  "https://openapi-provisioning.p.rapidapi.com/v1/apis/" +
-  core.getInput("rapidapi-api-id");
+try {
+  const API_URL =
+    "https://openapi-provisioning.p.rapidapi.com/v1/apis/" +
+    core.getInput("rapidapi-api-id");
+  const fileName = core.getInput("openapi-file");
 
-if (core.getInput("default-server-url") != "") {
-  // Update file with default server
-  // Multiple servers ?
+  if (core.getInput("default-server-url") != "") {
+    const file = require("./" + fileName);
+    file.servers = {
+      url: core.getInput("default-server-url"),
+    };
+    fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+      if (err) return console.log(err);
+      console.log(JSON.stringify(file));
+      console.log("Updating servers to " + fileName);
+    });
+  }
+
+  unirest
+    .put(API_URL)
+    .header("X-RapidAPI-Key", core.getInput("rapidapi-api-key"))
+    .header({ "Content-Type": "multipart/form-data" })
+    .attach("file", fileName)
+    .end(function (result) {
+      console.log(result.status, result.headers, result.body);
+    });
+} catch (error) {
+  core.setFailed(error.message);
 }
-
-unirest
-  .put(API_URL)
-  .header("X-RapidAPI-Key", core.getInput("rapidapi-api-key"))
-  .header({ "Content-Type": "multipart/form-data" })
-  .attach("file", core.getInput("openapi-file"))
-  .end(function (result) {
-    console.log(result.status, result.headers, result.body);
-  });
